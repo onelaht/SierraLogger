21267188f1a5974b6d2ee1ac64af45308f726638
onelaht <covercoverunder@gmail.com>
2026-03-30 10:12:30 -0400

    Implement queries for insertion

--- gin-server/queries.sql
+++ gin-server/queries.sql
@@ -1,18 +1,48 @@
 -- name: CreateAccount :one
 INSERT INTO accounts (
-    name, colDefs, tagDefs, rowData
+    account_name
 ) VALUES (
-    $1, $2, $3, $4
-)
-RETURNING *;
+    $1
+) RETURNING account_id;
 
--- name: GetAllAccounts :many
-SELECT * FROM accounts;
+-- name: CreateColDefConfig :one
+INSERT INTO column_def_config (
+    account_id, col_def_config
+) VALUES (
+    $1, $2
+) RETURNING  account_id;
 
--- name: GetAccountNames :many
-SELECT name FROM accounts;
+-- name: CreateTagDefConfig :one
+INSERT INTO tag_def_config (
+    account_id, tag_def_config
+) VALUES (
+    $1, $2
+) RETURNING account_id;
 
--- name: GetAccount :one
-SELECT *
-FROM accounts
-WHERE name = $1;
+-- name: CreateSierraData :one
+INSERT INTO sierra_data (
+    account_id, symbol, entry_datetime, trade_type, duration, profit_loss, cumulative_profit_loss,
+    max_open_profit, max_open_loss, exit_datetime, commission, max_open_quantity, trade_quantity, entry_price,
+    exit_price, max_closed_quantity, flat_to_flat_profit_loss, flat_to_flat_max_open_profit, flat_to_flat_max_open_loss,
+    entry_efficiency, total_efficiency, high_price_while_open, low_price_while_open, note, open_position_quantity,
+    close_position_quantity
+) VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9,
+    $10, $11, $12, $13, $14, $15, $16,
+    $17, $18, $19, $20, $21, $22,
+    $23, $24, $25, $26
+) RETURNING sierra_data_id;
+
+-- name: CreateTagDefs :one
+INSERT INTO tag_defs (
+    account_id, tag_name
+) VALUES (
+    $1, $2
+) RETURNING tag_def_id;
+
+-- name: CreateSierraDataTags :one
+INSERT INTO sierra_data_tags (
+    sierra_data_id, tag_def_id, tag_value
+) VALUES (
+    $1, $2, $3
+) RETURNING sierra_data_tag_id;
