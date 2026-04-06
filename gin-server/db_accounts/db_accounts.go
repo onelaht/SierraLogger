389ea3ff58821e7c5f99d13a5a241e6173cf2ed8
onelaht <covercoverunder@gmail.com>
2026-04-06 11:00:57 -0400

    Split SC and tag attributes

--- gin-server/db_accounts/db_accounts.go
+++ gin-server/db_accounts/db_accounts.go
@@ -1,8 +1,14 @@
 package db_accounts
 
 import (
+	"context"
+	"encoding/json"
+	"example/gin-server/db"
 	"example/gin-server/types"
 	"fmt"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 // NewAccount
@@ -42,32 +48,61 @@ func NewAccount(acc types.Account) error {
 		"FlatToFlat Max Open Profit (C)": struct{}{},
 		"Note":                           struct{}{},
 	}
-
+	// initialize context
+	ctx := context.Background()
+	// connect user and db
+	conn, err := pgx.Connect(ctx, "postgres://user1:pass@localhost:5432/db1_proj1?sslmode=disable")
+	if err != nil {
+		return err
+	}
+	defer conn.Close(ctx)
+	// create instance to execute queries
+	queries := db.New(conn)
 	// initialize db account
-	//
-
+	accID, _ := queries.CreateAccount(ctx, pgtype.Text{String: acc.AccName, Valid: true})
+	// serialize col def strut
+	colDefsMarshal, _ := json.Marshal(acc.ColDefs)
+	// create parameter type
+	colDefConfig := db.CreateColDefConfigParams{
+		AccountID:    accID,
+		ColDefConfig: colDefsMarshal,
+	}
 	// use account id to store ag grid coldef config
-	//
-
+	_, _ = queries.CreateColDefConfig(ctx, colDefConfig)
+	// serialize tag def struct
+	tagDefsMarshal, _ := json.Marshal(acc.TagDefs)
+	// create parameter type
+	tagDefConfig := db.CreateTagDefConfigParams{
+		AccountID:    accID,
+		TagDefConfig: tagDefsMarshal,
+	}
 	// use account id to store ag grid tagdef config
+	_, _ = queries.CreateTagDefConfig(ctx, tagDefConfig)
 	//
-
+	var attributes []map[string][]map[string]string = nil
 	// traverse per tuple
-	for i, tuple := range acc.RowData {
-		// use tuple index and create map where values {sierra: [], tags: []}
-		fmt.Println(i)
+	for _, tuple := range acc.RowData {
+		splitAtt := map[string][]map[string]string{
+			"sierra": nil,
+			"tags":   nil,
+		}
 		// traverse per attribute in tuple
 		for j := range tuple {
 			// add to sierra chart subset, if part of sierra chart standard att
 			if _, ok := sierraSet[j]; ok {
-				fmt.Println("This is part of Sierra Chart Statistics (" + j + ")")
+				splitAtt["sierra"] = append(splitAtt["sierra"], map[string]string{j: tuple[j]})
 				// add to user defined tags subset, if not part of sierra chart standard att
 			} else {
-				fmt.Println("This is part of Sierra Chart Statistics (" + j + ")")
+				splitAtt["tags"] = append(splitAtt["tags"], map[string]string{j: tuple[j]})
 			}
 		}
-		// add both query to chunk
+		attributes = append(attributes, splitAtt)
 	}
+	//
+	for i := range attributes {
+		fmt.Println(i)
+	}
+	// loop through split attributes arr
 
 	return nil
 }
